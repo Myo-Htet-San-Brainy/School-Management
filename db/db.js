21a29792f484d6ab3eb63206586f7c701a9853c0
Myo-Htet-San-Brainy <myohtetsandrinksgit@gmail.com>
2024-11-22 13:24:27 +0700

    store sensitive db credentials in env variables

--- db/db.js
+++ db/db.js
@@ -1,9 +1,9 @@
 const mysql = require("mysql2");
 
 const pool = mysql.createPool({
-  host: "educaseschool.cp0q68uyorz5.ap-southeast-1.rds.amazonaws.com",
-  user: "admin",
-  password: "Brainydrinksmilk1!",
+  host: process.env.HOST,
+  user: process.env.MASTER_USERNAME,
+  password: process.env.MASTER_PASSWORD,
   database: "School_Management",
   port: 3306,
   waitForConnections: true,
